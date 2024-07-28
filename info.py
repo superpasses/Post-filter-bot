80f1250ebbf8b1331395f2cf8ae95d1519245994
superpasses <165167006+superpasses@users.noreply.github.com>
2024-07-28 15:31:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,6 @@ API_HASH     = "0a708211226ff747f38c47a80a5e29c0"
 BOT_TOKEN    = "6517100010:AAHEzkYD9fGW-btrnkY16vPiAgT1tSv3lIE"
 SESSION      = "BQBDX_bqsrQw89oISrw0telsgo-5Je8QF50_ur790OKbps8lRYciEPAWQjNPM3Jjfsivbs0KGA4zfa1CSyw2dkH2k31eRRrOOoqQq6P1edSVPbRByo70SidrblFfDZnMZ_eyubcc5WK5Fv05nQNzdgZw5ljB0d6SctyIuWaraXYbaPtJaafdOJ2B7xOho-VElPdNRGPsSlPgD1hbAJmnIIPdubsQ87HQcX6H7PrM8xhPHRDN2_dVWZg5WnkyzZJ9-c-DWE4qJJHwnc4pYv9aaZGco_9K2zBOgoIWfoQVuuPUATZlj1qLODASB1Mjnsvo4fNDLJGJu-inmSCH9iTB-cixAAAAAb30djYA"
 DATABASE_URI = "mongodb+srv://deepchutia95:tmpGRbjdJE1WTpG8@cluster0.wrwzavb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1001884373226
+LOG_CHANNEL  = -1002167800454
 ADMIN        = 2107586423
 CHANNEL      = "@MKD_Korean_Drama"
